ea46d14e08da895df061b9bddf917df90ccab107
Phoenix51505 <88960080+Phoenix51505@users.noreply.github.com>
2026-04-29 16:14:02 -0400

    fix: Medical Alerts Are Back To Being Informative And Persistent

--- Content.Shared/Trigger/Components/Effects/RattleOnTriggerComponent.cs
+++ Content.Shared/Trigger/Components/Effects/RattleOnTriggerComponent.cs
@@ -18,13 +18,36 @@ public sealed partial class RattleOnTriggerComponent : BaseXOnTriggerComponent
     [DataField]
     public ProtoId<RadioChannelPrototype> RadioChannel = "Syndicate";
 
+
     /// <summary>
     /// The message to be send depending on the target's current mob state.
     /// </summary>
     [DataField]
     public Dictionary<MobState, LocId> Messages = new()
     {
-        {MobState.Critical, "rattle-on-trigger-critical-message"},
-        {MobState.Dead, "rattle-on-trigger-dead-message"}
+        {MobState.Critical, "deathrattle-implant-critical-message"},
+        {MobState.Dead, "deathrattle-implant-dead-message"} // Aurora's Song: Reverted these to the older, more informative locale
     };
+
+    // Aurora's Song.
+    /// <summary>
+    /// What time the user died, so Medics know how long they have been neglected for.
+    /// </summary>
+    [ViewVariables(VVAccess.ReadWrite), DataField]
+    public TimeSpan DeathTime = TimeSpan.Zero;
+
+    // Aurora's Song.
+    /// <summary>
+    /// What time the implant should next be retriggered automatically.
+    /// </summary>
+    [ViewVariables(VVAccess.ReadWrite), DataField]
+    public TimeSpan NextTrigger = TimeSpan.Zero;
+    
+
+    // Aurora's Song.
+    /// <summary>
+    /// The delay between implant retriggers.
+    /// </summary>
+    [ViewVariables(VVAccess.ReadWrite), DataField]
+    public TimeSpan RetriggerDelay = TimeSpan.FromMinutes(5);
 }
